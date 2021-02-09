6ebf6e79b8bd3c8dc2763ba946919bec60eb4c43
N11rm44L 7w44711 <60023424+niirmaaltwaatii@users.noreply.github.com>
2021-02-09 10:33:17 +0545

    extension options

---
+++ options/script.js
@@ -0,0 +1,24 @@
+/*
+GitHub : https://www.github.com/niirmaaltwaatii/BrowserLock
+Author : N11rm44l 7w44711
+*/
+
+var ch = document.querySelector("#ch");
+ch.addEventListener("click",chpwd);
+
+function chpwd(){
+    var lspwd = localStorage.getItem("pwd");
+    var oldpwd = document.querySelector("#oldpwd").value;
+    var newpwd = document.querySelector("#newpwd").value;
+    console.log(oldpwd)
+    if(oldpwd==lspwd){
+        if(newpwd != "" || null){
+            localStorage.setItem("pwd",newpwd);
+            alert("Password Changed !!!");
+        }else{
+            alert("New Password can't be Empty !!!")
+        }
+    }else{
+        alert("Incorrect old password !!!");
+    }
+}
